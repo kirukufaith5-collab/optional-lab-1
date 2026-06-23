0f050ace7fbe32a6c7e4c3c61862e7bc7d81a3c4
kirukufaith5-collab <kirukufaith5@gmail.com>
2026-06-23 11:31:42 +0300

    Fix spaces and spelling

--- server/app.py
+++ server/app.py
@@ -7,14 +7,14 @@ existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
 
 @app.route("/")
 def welcome ():
-    return "Welcome to Flatiron Cars "
+    return "Welcome to Flatiron Cars"
 
 @app.route("/<model>")
 def module(model):
     if model in existing_models:
         return f"Flatiron {model} is in our fleet!"
     else:
-        return f"No Model {model} exists in our catalog."
+        return f"No models called {model} exists in our catalog"
 
 if __name__ == '__main__':
     app.run(debug=True)
